57ba18f8634c71f656b9999a35d91988c10e53e4
madhur_malpani <madhurmm4u@gmail.com>
2018-04-04 11:20:26 +0530

    Made few fixes to the JSON parsing

--- insta-dl.py
+++ insta-dl.py
@@ -4,90 +4,93 @@ import urllib
 import os
 import json
 import urllib.request
+
 try:
-  import tkinter
+    import tkinter
 except ImportError:
-  import Tkinter as tkinter
+    import Tkinter as tkinter
 
 __version__ = "v.0.2.7"
 
 window = tkinter.Tk()
 
+
 def download(username):
-  request_url = "https://www.instagram.com/" + username + "?__a=1"
-  more_available = True
-  end_cursors = []
+    request_url = "https://www.instagram.com/" + username + "?__a=1"
+    more_available = True
+    end_cursors = []
 
-  headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0'
-  }
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0'
+    }
 
-  make_folder(entry.get()) # makes folder with given username
+    make_folder(entry.get())  # makes folder with given username
 
-  while more_available:
-    if not end_cursors:
-      response = requests.get(request_url, headers=headers)
-    
-    else:
-      response = requests.get(request_url + "&max_id={}".format(end_cursors[-1]))
+    while more_available:
+        if not end_cursors:
+            response = requests.get(request_url, headers=headers)
 
-    try:
-      data = response.json()
-    
-    except:
-      print("\033[91mInvalid username!\033[0m")
-      os.removedirs(entry.get())
-      break;
+        else:
+            response = requests.get(request_url + "&max_id={}".format(end_cursors[-1]))
 
-    for node in data["user"]["media"]["nodes"]:
-      # Cant access video url anymore
-      # if src["is_video"] and do_download_videos.get() == 1:
-      #   file_url = src["videos"]["standard_resolution"]["url"]
-      # else:
-      file_url = node["display_src"]
-      file_url = file_url.replace("s640x640","s1080x1080")
-      file_name = file_url.split("/")[-1]
-
-      path = entry.get() + "/" + username + "_" + file_name
-      
-      if not os.path.isfile(path):
-        
         try:
-          urllib.request.urlretrieve(file_url, path)
-          print("Downloaded: "+path)
-          sleep(0.5)
-
+            # data = response.json()
+            json_data = json.loads(response.text)
         except:
-          try:
-              urllib.urlretrieve(file_url, path)
-              print("Downloaded: "+path)
-              sleep(0.5)
+            print("\033[91mInvalid username!\033[0m")
+            os.removedirs(entry.get())
+            break;
 
-          except:
-              print("\033[91m----Skipping this image----\033[0m")
+        print(json_data.keys())
+        # for node in json_data["user"]["media"]["nodes"]:
+        for node in json_data['graphql']['user']['edge_owner_to_timeline_media']['edges']:
+            # Cant access video url anymore
+            # if src["is_video"] and do_download_videos.get() == 1:
+            #   file_url = src["videos"]["standard_resolution"]["url"]
+            # else:
+            file_url = node['node']['display_url']
+            # file_url = file_url.replace("s640x640", "s1080x1080")
+            file_name = file_url.split("/")[-1]
 
-    more_available = data["user"]["media"]["page_info"]["has_next_page"]
+            path = entry.get() + "/" + username + "_" + file_name
 
-    new_max_id = data["user"]["media"]["page_info"]["end_cursor"]
-    end_cursors.append(new_max_id)
+            if not os.path.isfile(path):
 
-    if more_available:
-      print("\033[92mGetting next page of images with maximum id: " + new_max_id + "\033[0m")
-    print("\033[92m--------------Completed--------------\033[0m")
+                try:
+                    urllib.request.urlretrieve(file_url, path)
+                    print("Downloaded: " + path)
+                    sleep(0.5)
+
+                except:
+                    try:
+                        urllib.urlretrieve(file_url, path)
+                        print("Downloaded: " + path)
+                        sleep(0.5)
+
+                    except:
+                        print("\033[91m----Skipping this image----\033[0m")
+
+        more_available = json_data["user"]["media"]["page_info"]["has_next_page"]
+
+        new_max_id = json_data["user"]["media"]["page_info"]["end_cursor"]
+        end_cursors.append(new_max_id)
+
+        if more_available:
+            print("\033[92mGetting next page of images with maximum id: " + new_max_id + "\033[0m")
+        print("\033[92m--------------Completed--------------\033[0m")
 
 
 def action():
-  download(entry.get())
+    download(entry.get())
 
 
 # Make folder with given username
 def make_folder(username):
-  try:
-    os.makedirs(username)
-  except OSError:
-    os.system("rm -rf " + username)
-    os.makedirs(username)
-
+    try:
+        os.makedirs(username)
+    except OSError:
+        os.system("rm -rf " + username)
+        os.makedirs(username)
 
 
 # Building the UI
@@ -97,15 +100,15 @@ window.geometry("300x200")
 window.resizable(False, False)
 
 entry = tkinter.Entry(window)
-entry.place(x=70,y=68)
+entry.place(x=70, y=68)
 entry.configure(highlightbackground="grey90")
 
 button = tkinter.Button(window, text="Download")
-button.place(x=110,y=120)
-button.configure(command=lambda:action(), highlightbackground="grey90")
+button.place(x=110, y=120)
+button.configure(command=lambda: action(), highlightbackground="grey90")
 
 notice = tkinter.Label(window, text="insta-dl is not affiliated with Instagram",
-                       fg="grey60",bg="grey90")
+                       fg="grey60", bg="grey90")
 notice.place(x=30, y=180)
 
 window.mainloop()
